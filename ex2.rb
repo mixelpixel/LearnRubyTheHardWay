b6d64f71ca063a2cd162e4d32eb43b2be8c980ab
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-03 11:40:35 -0400

    Add files via upload

--- ex2.rb
+++ ex2.rb
@@ -7,3 +7,10 @@ puts "I could have code like this." # and the comment after is ignored
 # puts "This won't run."
 
 puts "This will run."
+
+puts "Octothorpe's are #useful#"
+puts "I wonder what this will do in a puts:"
+puts "# -*- coding: utf-8 -*-"
+puts "...in a comment at the start of a script it will
+make sure Ruby reads the text file as utf-8"
+puts "Putz ...'puts' is gonna take some getting used to."
