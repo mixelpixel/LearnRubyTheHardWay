113e9f195cdb833af32da86af4d264293926b744
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-03 11:24:45 -0400

    Add files via upload

--- ex1.rb
+++ ex1.rb
@@ -5,3 +5,22 @@ puts "This if fun."
 puts "Yay! Printing"
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+puts "Here's another sentence."
+puts 'putz'
+puts 'Neat, the text editor I am using (Notepad++) shows "puts" with different colors according to the style of quote they use.'
+puts 'Neat, the text editor I am using (Notepad++) shows\n
+"puts" with different colors according to the\n
+style of quote they use.'
+puts "...That didn't work quite as expected"
+puts 'Neat, the text editor I am using (Notepad++) shows\n"puts" with different colors according to the\nstyle of quote they use.'
+puts 'Neat, the text editor I
+am using (Notepad++) shows
+"puts" with different colors
+according to the style of quote they use.'
+puts 'Hmmm ...interesting.'
+puts "line #1\
+    line #2\
+    line #3"
+puts "line #1
+    line #2
+    line #3"
