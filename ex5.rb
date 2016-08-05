843ee4b05e1f592f97c502dc3ab32e16d37ce28c
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-04 22:58:03 -0400

    Add files via upload
    figuring out text formatting option: .to_i, .to_f, .round(#)

--- ex5.rb
+++ ex5.rb
@@ -5,13 +5,35 @@ my_weight = 180 # lbs
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
+hair = my_hair.downcase
 
 puts "Let's talk about #{my_name}."
 puts "He's #{my_height} inches tall."
 puts "He's #{my_weight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "He's got #{my_eyes.downcase} eyes and #{hair} hair."
+puts "His teeth are usually #{my_teeth.upcase} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
 puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
+kilos = my_weight * 0.453592
+
+# rounds kilos variable
+puts "I weigh #{kilos.round(2)} kilograms."
+# rounds the conversion decimal used in calculation
+puts "I weigh #{my_weight * 0.453592.round(3)} kilograms."
+# rounds the result of the calculation
+puts "I weigh #{(my_weight * 0.453592).round(3)} kilograms."
+puts
+puts
+puts "Were those blank lines?"
+
+centimeters = my_height * 2.54
+
+puts "I am #{centimeters.round(2)} centimeters tall."
+puts "I am #{centimeters.round(1)} centimeters tall."
+puts "I am #{centimeters.round(0)} centimeters tall."
+# flooring
+puts "I am #{centimeters.to_i} centimeters tall."
+puts "I am #{(centimeters.to_i).to_f} centimeters tall."
