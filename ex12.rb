e3fc2d6c9b325ff947446b8e9fa134e44c0d50b7
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-07 04:51:18 -0400

    Add files via upload

--- ex12.rb
+++ ex12.rb
@@ -1,1 +1,30 @@
 # http://learnrubythehardway.org/book/ex12.html
+
+print "Give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+# now using to_f for a floating point number
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_f
+
+smaller = number / 100
+puts "A smaller decimal is #{smaller}."
+
+# calculate 15% tip
+print "Give me a total for your dinner bill: "
+bill = gets.chomp.to_i
+tip = bill * 0.15
+
+puts "A tip of #{tip.round(2)} would be nice"
