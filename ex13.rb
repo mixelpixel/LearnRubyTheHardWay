b23d4c4bd7b76eab75584512e9d90f0dfcff3ed8
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-07 06:29:17 -0400

    Add files via upload

--- ex13.rb
+++ ex13.rb
@@ -8,3 +8,25 @@ puts "Your third variable is: #{third}"
 
 # from the command line run this script like so:
 # ruby ex13.rb first second third
+# less variables just get nothing, more seem to be ignored
+
+print "Give me a number: "
+number = $stdin.gets.chomp
+
+bigger = number * 100
+puts "A big string is #{bigger}."
+
+print "Give me a number: "
+number = $stdin.gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+# What's the difference between ARGV and gets.chomp?
+# The difference has to do with where the user is required
+# to give input. If they give your script inputs on the
+# command line, then you use ARGV. If you want them to
+# input using the keyboard while the script is running, then use gets.chomp.
+
+# You should use $stdin.gets.chomp from now on since
+# the action gets.chomp has problems with ARGV.
