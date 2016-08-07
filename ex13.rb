8f53b5aa855e529ad37033f766e5b43952c966a3
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-07 06:01:51 -0400

    Add files via upload

--- ex13.rb
+++ ex13.rb
@@ -5,3 +5,6 @@ first, second, third = ARGV
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
+
+# from the command line run this script like so:
+# ruby ex13.rb first second third
