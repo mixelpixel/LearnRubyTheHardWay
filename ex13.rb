67a2c62f2b0bfd1fbcaa6ad18cd301d89c0802a4
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-07 06:01:01 -0400

    Add files via upload

--- ex13.rb
+++ ex13.rb
@@ -1,2 +1,7 @@
 # http://learnrubythehardway.org/book/ex13.html
 
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
