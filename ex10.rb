6baddbcf2e8f552970f95fb2fb8bfcb6d7689af1
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-07 02:48:07 -0400

    Add files via upload

--- ex10.rb
+++ ex10.rb
@@ -1,2 +1,26 @@
 # http://learnrubythehardway.org/book/ex10.html
 
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+# interesting that the single quotes ignore formatting except for the \' apostrophe
+slim_cat = '''
+I\'ll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+puts slim_cat
