b3b5af749020aae3fde611fdadf35bb1cd1d3077
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-12 02:08:19 -0400

    Add files via upload

--- ex21.rb
+++ ex21.rb
@@ -39,3 +39,39 @@ what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 puts "That becomes: #{what}. Can you do it by hand?"
 puts
 
+
+# sans returns... interesting
+
+def add(a, b)
+  puts "ADDING #{a} + #{b}"
+end
+
+def subtract(a, b)
+  puts "SUBTRACTING #{a} - #{b}"
+end
+
+def multiply(a, b)
+  puts "MULTIPLYING #{a} * #{b}"
+end
+
+def divide(a, b)
+  puts "DIVIDING #{a} / #{b}"
+end
+
+
+puts "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+puts "Here is a puzzle."
+
+# note how this gets unpacked having no return values in the functions:
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puts "That becomes: #{what}. Can you do it by hand?"
+puts
