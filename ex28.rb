1cc431f1f11bf185193c5f8a91bf4b0efe2b989b
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-13 19:22:13 -0400

    Add files via upload

--- ex28.rb
+++ ex28.rb
@@ -1,25 +1,25 @@
 # learnrubythehardway.org/book/ex28.html
 # Exercise 28: Boolean Practice
 
-# (true && true) == true
-# (false && true) == false
-# (1 == 1 && 2 == 1) == false
-# ("test" == "test") == true
-# (1 == 1 || 2 != 1) == true
-# (true && 1 == 1) == true 
-# (false && 0 != 0) == false
-# (true || 1 == 1) == true
-# ("test" == "testing") == false
-# (1 != 0 && 2) == 1
-# ("test" != "testing") == false
-# ("test" == 1) == false
-# (!(true && false)) == true
-# (!(1 == 1 && 0 != 1)) == false
-# (!(10 == 1 || 1000 == 1000)) == false
-# (!(1 != 10 || 3 == 4)) == false
-# (!("testing" == "testing" && "Zed" == "Cool Guy")) == true
-# (1 == 1 && (!("testing" == 1 || 1 == 0))) == true
-# ("chunky" == "bacon" && (!(3 == 4 || 3 == 3))) == false
-# (3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))) == false
+puts (true && true) == true
+puts (false && true) == false
+puts (1 == 1 && 2 == 1) == false
+puts ("test" == "test") == true
+puts (1 == 1 || 2 != 1) == true
+puts (true && 1 == 1) == true 
+puts (false && 0 != 0) == false
+puts (true || 1 == 1) == true
+puts ("test" == "testing") == false
+puts (1 != 0 && 2 == 1) == false # breezed through formatting brackets
+puts ("test" != "testing") == true # hah, didn't notice the !
+puts ("test" == 1) == false
+puts (!(true && false)) == true
+puts (!(1 == 1 && 0 != 1)) == false
+puts (!(10 == 1 || 1000 == 1000)) == false
+puts (!(1 != 10 || 3 == 4)) == false
+puts (!("testing" == "testing" && "Zed" == "Cool Guy")) == true
+puts (1 == 1 && (!("testing" == 1 || 1 == 0))) == true
+puts ("chunky" == "bacon" && (!(3 == 4 || 3 == 3))) == false
+puts (3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))) == false
 
 
