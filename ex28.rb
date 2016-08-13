598677fd61ed11af222213f332fb2e4037e9af23
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-13 19:19:16 -0400

    Add files via upload

--- ex28.rb
+++ ex28.rb
@@ -1,23 +1,25 @@
 # learnrubythehardway.org/book/ex28.html
 # Exercise 28: Boolean Practice
 
-# true && true
-# false && true
-# 1 == 1 && 2 == 1
-# "test" == "test"
-# 1 == 1 || 2 != 1
-# true && 1 == 1
-# false && 0 != 0
-# true || 1 == 1
-# "test" == "testing"
-# 1 != 0 && 2 == 1
-# "test" != "testing"
-# "test" == 1
-# !(true && false)
-# !(1 == 1 && 0 != 1)
-# !(10 == 1 || 1000 == 1000)
-# !(1 != 10 || 3 == 4)
-# !("testing" == "testing" && "Zed" == "Cool Guy")
-# 1 == 1 && (!("testing" == 1 || 1 == 0))
-# "chunky" == "bacon" && (!(3 == 4 || 3 == 3))
-# 3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
+# (true && true) == true
+# (false && true) == false
+# (1 == 1 && 2 == 1) == false
+# ("test" == "test") == true
+# (1 == 1 || 2 != 1) == true
+# (true && 1 == 1) == true 
+# (false && 0 != 0) == false
+# (true || 1 == 1) == true
+# ("test" == "testing") == false
+# (1 != 0 && 2) == 1
+# ("test" != "testing") == false
+# ("test" == 1) == false
+# (!(true && false)) == true
+# (!(1 == 1 && 0 != 1)) == false
+# (!(10 == 1 || 1000 == 1000)) == false
+# (!(1 != 10 || 3 == 4)) == false
+# (!("testing" == "testing" && "Zed" == "Cool Guy")) == true
+# (1 == 1 && (!("testing" == 1 || 1 == 0))) == true
+# ("chunky" == "bacon" && (!(3 == 4 || 3 == 3))) == false
+# (3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))) == false
+
+
