965443104925f630a7d353ff169136de3b3b5578
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-14 06:58:05 -0400

    Add files via upload

--- ex35.rb
+++ ex35.rb
@@ -1,6 +1,8 @@
 # http://learnrubythehardway.org/book/ex35.html
 # Exercise 35: Branches and Functions
 
+
+# Line 7 "bug" debugging.
 choice = "50"
 
 if choice.include?("0") || choice.include?("1")
@@ -11,3 +13,10 @@ end
 
 # a-ha, thank you internets
 # http://stackoverflow.com/questions/29308499/bug-in-if-statement-parameters-learn-ruby-the-hard-way
+
+
+# nice way to simplify
+
+def prompt
+  print "> "
+end
