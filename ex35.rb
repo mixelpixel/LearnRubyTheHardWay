a0428bf144b28488f85d1c1d74aaab79126194fc
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-14 06:55:11 -0400

    Add files via upload

--- ex35.rb
+++ ex35.rb
@@ -1,4 +1,13 @@
 # http://learnrubythehardway.org/book/ex35.html
 # Exercise 35: Branches and Functions
 
+choice = "50"
 
+if choice.include?("0") || choice.include?("1")
+  puts "what?"
+end
+
+# Hmmmm?
+
+# a-ha, thank you internets
+# http://stackoverflow.com/questions/29308499/bug-in-if-statement-parameters-learn-ruby-the-hard-way
