a513014b76f1e04ff0dd5af39f73733c01db39d1
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-14 05:33:34 -0400

    Add files via upload

--- ex33.rb
+++ ex33.rb
@@ -2,19 +2,34 @@
 # Exercise 33: While Loops
 
 i = 0
-numbers = []
+numbers = [] # EMPTY
 
 while i < 6
   puts "At the top is #{i}"
-  numbers.push(i)
+  numbers.push(i) # ADDS TO THE END OF THE ARRAY
   
   i += 1
-  puts "Numbers now: ", numbers
+  # puts "Numbers now: ", numbers # puts prints each [array]s element
+  p numbers # p prints the whole [array]
   puts "At the bottom i is #{i}"
+  puts "\n"
 end
 
-puts "The numbers: "
+puts "The numbers:"
 
 # remember you can write this 2 other ways?
 numbers.each {|num| puts num }
 
+# The for-loop way
+puts "for-loop:"
+for num in numbers
+  p num
+  # p numbers
+end
+
+# The each iterator with "do"
+puts "each-do:"
+numbers.each do |num|
+  p num
+  # p numbers
+end
