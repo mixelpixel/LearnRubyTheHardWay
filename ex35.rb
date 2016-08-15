520e06c3a73aebd6da1b5b44c0b661c7d7afe1a7
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-15 00:38:39 -0400

    Add files via upload

--- ex35.rb
+++ ex35.rb
@@ -1,35 +1,35 @@
 # http://learnrubythehardway.org/book/ex35.html
 # Exercise 35: Branches and Functions
 
-
 # # Line 7 "bug" debugging.
 # choice = "50"
-
 # if choice.include?("0") || choice.include?("1")
   # puts "what?"
 # end
-
 # # Hmmmm?
-
 # # a-ha, thank you internets
 # # http://stackoverflow.com/questions/29308499/bug-in-if-statement-parameters-learn-ruby-the-hard-way
+# # Ruby "pattern match" operator =~
+# # http://stackoverflow.com/questions/3025838/what-is-the-operator-in-ruby
 
 
 # # nice way to simplify
-
-# def prompt
-  # print "> "
-# end
+def prompt
+  print "> "
+end
 
 
 def gold_room
   puts "This room is full of gold. how much do you take?"
   
-  print "> "
-  choice = $stdin.gets.chomp
+  # print "> "
+  # choice = $stdin.gets.chomp
+  prompt; choice = $stdin.gets.chomp # NOTE the use of the semi-colon
   
   #this line has a bug, so fix it (bug = does not recognize 0-9, only 1 & 0)
-  if choice.include?("0") || choice.include?("1")
+  # if choice.include?("0") || choice.include?("1")
+  # Regular expressions: \d means any Digit, + means one or more times and =~ is a pattern match operator
+  if choice =~ /\d+/
     how_much = choice.to_i
   else
     dead("Man, learn to type a number.")
@@ -45,21 +45,18 @@ end
 
 
 def bear_room
-  puts "There is a bear here."
-  puts "The bear has a bunch of honey."
-  puts "The fat bear is in front of another door."
-  puts "How are you going to move the bear?"
-  # puts """
-# There is a bear here.
-# The bear has a bunch of honey.
-# The fat bear is in front of another door.
-# How are you going to move the bear?
-# """
+  puts """
+There is a bear here.
+The bear has a bunch of honey.
+The fat bear is in front of another door.
+How are you going to move the bear?
+"""
   bear_moved = false
   
   while true
-    print "> "
-    choice = $stdin.gets.chomp
+    # print "> "
+    # choice = $stdin.gets.chomp
+    prompt; choice = $stdin.gets.chomp
     
     if choice == "take honey"
       dead("The bear looks at you then slaps your face off.")
@@ -77,15 +74,16 @@ def bear_room
 end
 
 
-def cthulu_room
+def cthulhu_room
   puts """
 Here you see the great evil Cthulhu.
 He, it, whatever stares at you and you go insane.
 Do you flee for your life or eat your head?
 """
 
-  print "> "
-  choice = $stdin.gets.chomp
+  # print "> "
+  # choice = $stdin.gets.chomp
+  prompt; choice = $stdin.gets.chomp
   
   if choice.include? "flee"
     start
@@ -109,8 +107,9 @@ There is a door to your right and left.
 Which one do you take?
 """
 
-  print "> "
-  choice = $stdin.gets.chomp
+  # print "> "
+  # choice = $stdin.gets.chomp
+  prompt; choice = $stdin.gets.chomp
   
   if choice == "left"
     bear_room
