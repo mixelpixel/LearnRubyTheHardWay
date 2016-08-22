778f7721a4624d6b130fa53d3d4296b99c51af24
Patrick Kennedy <mixelpix@mac.com>
2016-08-21 23:27:05 -0400

    implicit inheritance

--- ex44.rb
+++ ex44.rb
@@ -1,3 +1,22 @@
 # http://learnrubythehardway.org/book/ex44.html
 # Exercise 44: Inheritance Versus Composition
 
+# Implicit Inheritance
+
+class Parent
+
+  def implicit()
+    puts "PARENT implicit()"
+  end
+end
+
+
+class Child < Parent
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.implicit()
+son.implicit()
+
