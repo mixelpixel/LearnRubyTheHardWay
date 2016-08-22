c779ec92b25daea52cf06fed5afeb0f524d567f3
Patrick Kennedy <mixelpix@mac.com>
2016-08-21 23:33:59 -0400

    override explicitly

--- ex44.rb
+++ ex44.rb
@@ -20,3 +20,27 @@ son = Child.new()
 dad.implicit()
 son.implicit()
 
+
+# Override Explicitly
+
+class Parent
+
+  def override()
+    puts "PARENT override()"
+  end
+end
+
+
+class Child < Parent
+  def override()
+    puts "CHILD override()"
+  end
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.override()
+son.override()
+
+
