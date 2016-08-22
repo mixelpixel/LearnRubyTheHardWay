9d8f99367b6b4b3381a1e2627bb2b3515996b036
Patrick Kennedy <mixelpix@mac.com>
2016-08-22 00:08:01 -0400

    all three combined

--- ex44.rb
+++ ex44.rb
@@ -18,8 +18,10 @@ end
 dad = Parent.new()
 son = Child.new()
 
+puts "IMPLICIT INHERITANCE"
 dad.implicit()
 son.implicit()
+puts ""
 
 
 # Override Explicitly
@@ -41,9 +43,10 @@ end
 dad = Parent.new()
 son = Child.new()
 
+puts "OVERRIDE EXPLICITLY"
 dad.override()
 son.override()
-
+puts ""
 
 # Alter Before or After
 
@@ -64,8 +67,61 @@ end
 dad = Parent.new()
 son = Child.new()
 
+puts "ALTER BEFORE OR AFTER"
+dad.altered()
+son.altered()
+puts""
+
+
+# All Three Combined
+
+class Parent
+
+  def override()
+    puts "PARENT override()"
+  end
+  
+  def implicit()
+    puts "PARENT implicit()"
+  end
+  
+  def altered()
+    puts "PARENT altered()"
+  end
+end
+
+
+class Child < Parent
+
+  def override()
+    puts "CHILD override()"
+  end
+  
+  def altered()
+    puts "CHILD, BEFORE PARENT altered()"
+    super()
+    puts "CHILD, AFTER PARENT altered()"
+  end
+end
+
+dad = Parent.new()
+son = Child.new()
+
+puts "ALL THREE COMBINED"
+
+dad.implicit()
+son.implicit()
+
+puts ""
+
+dad.override()
+son.override()
+
+puts ""
+
 dad.altered()
 son.altered()
 
+puts ""
 
 
